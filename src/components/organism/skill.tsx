886f49d10f5cd4d4dd4becfe9e0e28da6216608e
AnamSadat <anamsadat3@gmail.com>
2025-09-22 15:19:19 +0700

    feat: add components skill and experience

--- src/components/organism/skill.tsx
+++ src/components/organism/skill.tsx
@@ -1,8 +1,28 @@
-export function Skill() {
+import { ClassNameProps } from '@/types';
+import clsx from 'clsx';
+import { Card } from '../ui';
+
+type SkillProps = ClassNameProps;
+
+export function Skill({ className }: SkillProps) {
+  const baseClass = clsx(className);
+  const baseCard = clsx('p-5 bg-[#282830]');
   return (
-    <div>
-      <div>
-        <h1>Skill</h1>
+    <div className={baseClass}>
+      <header className="text-center ">
+        <h1 className="text-5xl font-bold">About Me</h1>
+        <p className="text-lg pt-5 text-[#d9fef0]">
+          Mengenal lebih dekat sosok di balik karya-karya digital
+        </p>
+      </header>
+      <div className="grid grid-cols-3 gap-5 pt-10">
+        <Card className={baseCard}>Saya belajar</Card>
+        <Card className={baseCard}>Saya belajar</Card>
+        <Card className={baseCard}>Saya belajar</Card>
+      </div>
+      <div className="flex mx-auto justify-center gap-5 mt-5">
+        <Card className={`w-md ${baseCard}`}>Saya belajar</Card>
+        <Card className={`w-md ${baseCard}`}>Saya belajar</Card>
       </div>
     </div>
   );
