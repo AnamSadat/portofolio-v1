4def77ca293e9e1243ac15ed970c8bbb0db394ba
AnamSadat <anamsadat3@gmail.com>
2025-09-22 15:19:46 +0700

    refactor: update style

--- src/components/organism/about-me.tsx
+++ src/components/organism/about-me.tsx
@@ -8,20 +8,23 @@ import {
   CardHeader,
 } from '@/components/ui';
 import { MapPin } from 'lucide-react';
+import Image from 'next/image';
 
 export function AboutMe({ className }: ClassNameProps) {
   const baseClass = clsx(className);
+
+  const baseCard = clsx('bg-[#282830]');
   return (
     <div className={baseClass}>
       <header className="text-center ">
-        <h1 className="text-5xl font-bold">About Me</h1>
-        <p className="text-lg pt-5">
+        <h1 className="text-5xl font-bold text-[#ff0099]">About Me</h1>
+        <p className="text-lg pt-5 text-[#d9fef0]">
           Mengenal lebih dekat sosok di balik karya-karya digital
         </p>
       </header>
-      <div className="grid grid-cols-2 text-center gap-12">
-        <div className="py-20 grid grid-rows-1 gap-5">
-          <Card>
+      <div className="grid grid-cols-2 text-center ">
+        <div className="pt-20 grid grid-rows-1 gap-5">
+          <Card className={baseCard}>
             <CardHeader>Porfile saya</CardHeader>
             <CardContent>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -32,8 +35,8 @@ export function AboutMe({ className }: ClassNameProps) {
               <MapPin /> Kuningan, Jawa Barat
             </CardFooter>
           </Card>
-          <Card>
-            <h1>Minat dan keahlian</h1>
+          <Card className={baseCard}>
+            <h1 className="text-[#b3b3b3]">Minat dan keahlian</h1>
             <div className="flex gap-3">
               <Badge variant={'outline'} className="rounded-2xl">
                 saya
@@ -53,12 +56,23 @@ export function AboutMe({ className }: ClassNameProps) {
             </div>
           </Card>
         </div>
-        <div className="py-20 gap-5 grid grid-rows-1">
-          <Card> selamat datang</Card>
-          <Card>selamat datang</Card>
-          <Card>selamat datang</Card>
-          <Card>Quotes Keren</Card>
+        <div className="pt-20 mx-auto">
+          <Card className={`p-5 rounded-2xl ${baseCard}`}>
+            <Image
+              src={'/anam.jpg'}
+              alt="belajar"
+              className="rounded-2xl"
+              width={350}
+              height={0}
+            />
+          </Card>
         </div>
+      </div>
+      <div className="pt-20 gap-5 grid grid-cols-4">
+        <Card className={baseCard}> selamat datang</Card>
+        <Card className={baseCard}>selamat datang</Card>
+        <Card className={baseCard}>selamat datang</Card>
+        <Card className={baseCard}>Quotes Keren</Card>
       </div>
     </div>
   );
