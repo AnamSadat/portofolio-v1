e2c6bfef4090a0aec86dbff81a1da53ef940eccb
AnamSadat <anamsadat3@gmail.com>
2025-10-03 12:22:51 +0700

    add milestone to about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,31 @@ import {
   Portofolio,
   Contact,
 } from '@/components/organism';
-import { ChevronsDown } from 'lucide-react';
+import { Archive, Award, Briefcase, ChevronsDown, Smile } from 'lucide-react';
 
 export default function Home() {
+  const myMilestones = [
+    {
+      icon: <Briefcase />,
+      title: '2+',
+      description: 'Tahun Pengalaman',
+    },
+    {
+      icon: <Archive />,
+      title: '10+',
+      description: 'Proyek Selesai',
+    },
+    {
+      icon: <Smile />,
+      title: '5+',
+      description: 'Klien Puas',
+    },
+    {
+      icon: <Award />,
+      title: '3+',
+      description: 'Sertifikasi',
+    },
+  ];
   return (
     <div className="container mx-auto  items-center w-full py-10">
       <HeroSection
@@ -18,7 +40,7 @@ export default function Home() {
         icon={<ChevronsDown className="mx-auto" />}
         classNameTitle=""
       />
-      <AboutMe className="pb-10 pt-20" />
+      <AboutMe className="pb-20 pt-20" milestones={myMilestones} />
       <Skill />
       <Experience />
       <Portofolio className="py-10" />
