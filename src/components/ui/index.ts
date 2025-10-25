f9cbfec14c108e1e0eb4c6888f9439fb7be2b927
AnamSadat <anamsadat3@gmail.com>
2025-10-25 23:19:58 +0700

    feat: add toggle to components/ui and import with index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -7,3 +7,5 @@ export * from '@/components/ui/dropdown-menu';
 export * from '@/components/ui/input';
 export * from '@/components/ui/label';
 export * from '@/components/ui/badge';
+export * from '@/components/ui/toggle';
+export * from '@/components/ui/toggle-group';
