dd1e275ff35146afacd74d3aa897104df09421ba
AnamSadat <anamsadat3@gmail.com>
2025-11-24 00:38:20 +0700

    refactor: create constan for reusble className

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,4 @@
 export const isDark = 'dark:bg-[#1c1c22]';
-export const green = 'text-[#00ff99]';
+export const isGreen = 'text-[#00ff99]';
 export const isBlack = 'bg-[#1c1c22]';
+export const isColorCard = 'bg-[#202026c1]';
