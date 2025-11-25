e92573bced1b49a137af5d28d51e23f958c6147f
AnamSadat <anamsadat3@gmail.com>
2025-11-25 09:29:02 +0700

    feat: add ui components pagination

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -11,3 +11,4 @@ export * from '@/components/ui/toggle';
 export * from '@/components/ui/toggle-group';
 export * from '@/components/ui/field';
 export * from '@/components/ui/separator';
+export * from '@/components/ui/pagination';
