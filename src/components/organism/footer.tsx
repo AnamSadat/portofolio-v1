35e88b69990573345631c4c239f6bbece167bd99
AnamSadat <anamsadat3@gmail.com>
2025-11-30 14:47:32 +0700

    refactor: change url to terms

--- src/components/organism/footer.tsx
+++ src/components/organism/footer.tsx
@@ -197,7 +197,7 @@ export function Footer() {
               <Link href="/privacy-policy" className="hover:text-gray-300">
                 Privacy Policy
               </Link>
-              <Link href="/term-condition" className="hover:text-gray-300">
+              <Link href="/terms-of-service" className="hover:text-gray-300">
                 Terms of Service
               </Link>
             </div>
