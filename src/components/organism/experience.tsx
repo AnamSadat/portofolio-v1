928e92efa9f8e4dc129919d4b55c09ddc0b0bf02
AnamSadat <anamsadat3@gmail.com>
2025-11-30 08:28:18 +0700

    feat: responsive experience

--- src/components/organism/experience.tsx
+++ src/components/organism/experience.tsx
@@ -25,7 +25,7 @@ const dotColor = '#00ff99';
 
 export function Experience({ className }: ExperienceProps) {
   const ref = useRef(null);
-  const baseClass = cn(className);
+  const baseClass = cn('w-full mx-auto px-10 sm:px-6 lg:px-0', className);
 
   return (
     <>
@@ -34,7 +34,7 @@ export function Experience({ className }: ExperienceProps) {
           <Header
             title="Expe"
             titleColor="rience"
-            className="text-5xl font-bold text-center text-white"
+            className="text-4xl sm:text-4xl md:text-5xl font-bold text-center text-white"
             classNameTitleColor="text-custom"
           />
           <Description>
@@ -43,32 +43,35 @@ export function Experience({ className }: ExperienceProps) {
           </Description>
         </header>
 
-        <div className="relative">
+        <div className="relative mt-10">
           {/* Garis background */}
-          <div className="absolute left-[18px] top-2 bottom-1 w-[3px] bg-[#00ff99]/60" />
+          <div className="hidden md:block absolute left-[14px] sm:left-[18px] top-3 bottom-3 w-[2px] sm:w-[3px] bg-[#00ff99]/60" />
 
           {/* Items */}
-          <div className="space-y-10 mt-16">
+          <div className="space-y-8 sm:space-y-10 mt-8 sm:mt-12">
             {timeLine.map((item, i) => (
               <TimelineItem key={i} item={item} color={dotColor} />
             ))}
           </div>
         </div>
       </div>
-      {/* 1d1d22 */}
-      <Persuasif
-        className={cn(
-          'text-center border-2',
-          'text-center border-2 border-emerald-700 bg-emerald-950/30'
-        )}
-        classNameLayout={cn('py-8')}
-        title="Siap untuk"
-        classNameTitle="text-2xl font-bold"
-        span="Kolaborasi?"
-        classNameSpan="text-emerald-500"
-        description="Mari berdiskusi tentang proyek selanjutnya dan bagaimana saya dapat membantu mewujudkan visi digital Anda."
-        classNameDescription="max-w-2xl mx-auto"
-      />
+
+      {/* Section ajakan kolaborasi */}
+      <div className="px-10 md:px-0">
+        <Persuasif
+          className={cn(
+            'text-center border-2 sm:mt-14 px-4',
+            'border-emerald-700 bg-emerald-950/30'
+          )}
+          classNameLayout={cn('py-8 mx-auto')}
+          title="Siap untuk"
+          classNameTitle="text-2xl sm:text-3xl font-bold"
+          span="Kolaborasi?"
+          classNameSpan="text-emerald-500"
+          description="Mari berdiskusi tentang proyek selanjutnya dan bagaimana saya dapat membantu mewujudkan visi digital Anda."
+          classNameDescription="max-w-2xl mx-auto text-sm sm:text-base"
+        />
+      </div>
     </>
   );
 }
